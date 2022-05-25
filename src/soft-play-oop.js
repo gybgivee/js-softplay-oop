a9a76897398f802a9f38973654354e37ae4a920d
gybgivee <kmcn0512@gmail.com>
2022-05-25 11:27:54 +0100

    update

--- src/soft-play-oop.js
+++ src/soft-play-oop.js
@@ -1,9 +1,9 @@
-// TODO: Write your class in this file
 class SoftPlay {
   constructor(children = 0, adults = 0) {
     this.children = children;
     this.adults = adults;
   }
+
   reset() {
     this.adults = 0;
     this.children = 0;
@@ -15,80 +15,86 @@ class SoftPlay {
       adults: this.adults,
     };
   }
-  maxOccupancy(total) {
+
+  maxOccupancy(total = 0) {
     this.total = total;
     console.log("this.total :" + this.total);
   }
   getTotal() {
     return this.total;
   }
+  resetTotal() {
+    this.total = 0;
+  }
   childrenPerAnAdult(maxChildren) {
     this.maxChildren = maxChildren;
-
     console.log("this.maxChildren :" + this.maxChildren);
   }
   getMaxChildren() {
     return this.maxChildren;
   }
+  resetChildrenPerAnAdult() {
+    this.maxChildren = 0;
+  }
+
   enter(numAdults, numChilds) {
     const totalEnter = numAdults + numChilds;
     const total = this.getTotal();
-
+    this.resetTotal();
+    console.log("totalEnter:" + totalEnter + "total:" + total);
     const maxChildren = this.getMaxChildren();
     const maxChildrenEnter = maxChildren * numAdults;
-    //console.log('maxChildren :'+maxChildren+' maxChildrenEnter :'+maxChildrenEnter+' numChilds :'+numChilds);
 
-    if (
-      totalEnter <= total ||
-      total === undefined ||
-      maxChildrenEnter >= numChilds ||
-      maxChildren === undefined
-    ) {
-      if (
-        (numAdults < numChilds && maxChildrenEnter < numChilds) ||
-        numAdults === 0
-      ) {
-        //console.log('numAdults < numChilds');
-        return false;
-      } else {
+    if (total === undefined && maxChildren === undefined) {
+      if (numAdults >= numChilds) {
         this.adults = this.adults + numAdults;
         this.children = this.children + numChilds;
 
         return true;
+      } else {
+        return false;
       }
+    } else if (
+      totalEnter <= total ||
+      (maxChildrenEnter >= numChilds && numAdults < numChilds && numAdults != 0)
+    ) {
+      this.adults = this.adults + numAdults;
+      this.children = this.children + numChilds;
+
+      return true;
     } else {
       return false;
     }
   }
   leave(numAdults, numChilds) {
-
     const childrenRemain = this.children - numChilds;
     const adultsRemain = this.adults - numAdults;
-    const checkChildAlone = numChilds - numAdults;
 
     const maxChildren = this.getMaxChildren();
     const maxChildrenLeave = maxChildren * numAdults;
     //console.log("numAdults  :" +numAdults +" numChilds :" +numChilds +" maxChildren :" +maxChildren +" maxChildrenLeave :" +maxChildrenLeave);
-
-    if (maxChildrenLeave >= numChilds || maxChildren === undefined) {
-
-      if (
-        numAdults >= numChilds &&
-        numChilds <= this.children &&
-        numAdults <= this.adults &&
-        adultsRemain >= childrenRemain
-      ) {
-        this.adults = adultsRemain;
-        this.children = childrenRemain;
-        return true;
-      } else if (numAdults < numChilds && maxChildrenLeave >= numChilds) {
-        this.adults = adultsRemain;
-        this.children = childrenRemain;
-        return true;
-      } else {
-        return false;
-      }
-
+    if (maxChildren === undefined) {
+          if (
+            numAdults >= numChilds &&
+            numChilds <= this.children &&
+            numAdults <= this.adults &&
+            adultsRemain >= childrenRemain
+          ) {
+            this.adults = adultsRemain;
+            this.children = childrenRemain;
+            return true;
+          }
+          else{
+            return false;;
+          }
+    } else if (
+      maxChildrenLeave >= numChilds &&
+      numAdults < numChilds &&
+      numAdults != 0
+    ){
+      this.adults = adultsRemain;
+      this.children = childrenRemain;
+      return true;
     } else {
       return false;
     }
@@ -96,20 +102,28 @@ class SoftPlay {
 }
 
 const smallSoftPaly = new SoftPlay();
-smallSoftPaly.maxOccupancy(2);
-
-console.log("Enter After Checked Max Occupancy -> " + smallSoftPaly.enter(2, 1));
-
-smallSoftPaly.childrenPerAnAdult(2);
-console.log("Enter After Checked childrenPerAnAdult-> " + smallSoftPaly.enter(1, 2));
-smallSoftPaly.childrenPerAnAdult(1);
-console.log("Enter After Checked childrenPerAnAdult-> " + smallSoftPaly.enter(1, 2));
+smallSoftPaly.maxOccupancy(1);
+console.log(
+  "\nEnter After Checked Max Occupancy -> " + smallSoftPaly.enter(2, 1)
+);
 
 smallSoftPaly.childrenPerAnAdult(2);
-console.log("Leave After Checked childrenPerAnAdult-> " + smallSoftPaly.leave(1, 2));
+console.log(
+  "\nEnter After Checked childrenPerAnAdult-> " + smallSoftPaly.enter(1, 2)
+);
 smallSoftPaly.childrenPerAnAdult(1);
-console.log("Leave After Checked childrenPerAnAdult-> " + smallSoftPaly.leave(1, 2));
+console.log(
+  "\nEnter After Checked childrenPerAnAdult-> " + smallSoftPaly.enter(1, 2)
+);
 
+smallSoftPaly.childrenPerAnAdult(2);
+console.log(
+  "\nLeave After Checked childrenPerAnAdult-> " + smallSoftPaly.leave(1, 2)
+);
+smallSoftPaly.childrenPerAnAdult(1);
+console.log(
+  "\nLeave After Checked childrenPerAnAdult-> " + smallSoftPaly.leave(1, 2)
+);
 
 // TODO: Change undefined to the name of your class
 module.exports = {
